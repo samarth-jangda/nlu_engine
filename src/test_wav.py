db08771b27ef8a4c3dc6f22cc69f09e367f1f487
Samarth Jangda <111646337+DevAcrobats0@users.noreply.github.com>
2024-04-06 12:20:09 +0530

    code to save transcript

--- src/test_wav.py
+++ src/test_wav.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 
 import asyncio
+import json
 import websockets
 import sys
 import wave
 
+transcript=[]
 async def run_test(uri):
     async with websockets.connect(uri) as websocket:
+
         wf = wave.open(sys.argv[1], "rb")
+        print(wf.getframerate())
         words=["हाँ","ना","नहीं"]
         # await websocket.send('{ "config" : { "sample_rate" : %d } }' % (wf.getframerate()))
         # await websocket.send('{"config":{ "sample_rate" : 8000, "words":["हाँ","ना","नहीं"]} }')
-        buffer_size = int(wf.getframerate() * 0.25) # 0.25 seconds of audio
+        buffer_size = int(wf.getframerate() * 0.20) # 0.19 seconds of audio
         while True:
             data = wf.readframes(buffer_size)
 
@@ -19,9 +23,23 @@ async def run_test(uri):
                 break
 
             await websocket.send(data)
-            print (await websocket.recv())
-
+            res=await websocket.recv()
+            if 'text' in res:
+                json_value=json.loads(res)
+                print(json_value.get("text"))
+                transcript.append(json_value.get("text"))
         await websocket.send('{"eof" : 1}')
-        print (await websocket.recv())
-
+        
+        final_res=await websocket.recv()
+        if 'text' in final_res:
+            json_value=json.loads(final_res)
+            transcript.append(json_value.get("text"))
+            print(json_value.get("text"))
+            
+        # write data to a text file
+        file_path="/home/samarthjangda/testing/salesphony_testing/prediction.txt"
+        transcript.pop(0)
+        with open(file_path, 'w') as pred_file:
+            for data in transcript:
+                pred_file.writelines(f"{data}\n")
 asyncio.run(run_test('ws://localhost:2700'))
